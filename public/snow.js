f03d2dc86824b2106ec846b3014ee69b2765757e
Ionică Bizău <bizauionica@yahoo.com>
2013-12-30 12:05:29 +0200

    count and windPower change

--- public/snow.js
+++ public/snow.js
@@ -2,19 +2,19 @@ $(document).ready( function() {
 
     // init let it snow
     $("canvas.snow").let_it_snow({
-        windPower: -20,
+        windPower: 0,
         speed: 1,
-        count: 250,
+        count: 300,
         size: 0,
         interaction: false
     });
 
     // on swipe left
     $("body").on("webcamSwipeLeft", function () {
-        $("canvas.snow").trigger("letItSnow.set", ["windPower", -20]);
+        $("canvas.snow").trigger("letItSnow.set", ["windPower", -25]);
     // on swipe right
     }).on("webcamSwipeRight", function () {
-        $("canvas.snow").trigger("letItSnow.set", ["windPower", 20]);
+        $("canvas.snow").trigger("letItSnow.set", ["windPower", 25]);
     });
 });
 
