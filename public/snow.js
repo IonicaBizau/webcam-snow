727999a54cd3960d33a466a7c16ad60883c6ffac
Ionică Bizău <bizauionica@yahoo.com>
2013-12-30 10:04:43 +0200

    Initial commit

---
+++ public/snow.js
@@ -0,0 +1,10 @@
+$(document).ready( function() {
+
+    $("canvas.snow").let_it_snow({
+        windPower: -3,
+        speed: 1,
+        count: 500,
+        size: 0,
+        interaction: false
+    });
+});
