bd589848beef04bacd03b516ae77a98db522b7b8
Ionică Bizău <bizauionica@yahoo.com>
2013-12-30 11:48:40 +0200

    Change the wind power on hand move.

--- public/snow.js
+++ public/snow.js
@@ -1,18 +1,22 @@
 $(document).ready( function() {
 
+    // init let it snow
     $("canvas.snow").let_it_snow({
-        windPower: -3,
+        windPower: -20,
         speed: 1,
-        count: 500,
+        count: 250,
         size: 0,
         interaction: false
     });
 
+    // on swipe left
     $("body").on("webcamSwipeLeft", function () {
-
+        $("canvas.snow").trigger("letItSnow.set", ["windPower", -20]);
+    // on swipe right
     }).on("webcamSwipeRight", function () {
-
+        $("canvas.snow").trigger("letItSnow.set", ["windPower", 20]);
     });
 });
 
+// init the webcam swiper
 window.initializeWebcamSwiper();
