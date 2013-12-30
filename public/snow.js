361dbe9053281dd75ee6aca33f8a0e9287203c33
Ionică Bizău <bizauionica@yahoo.com>
2013-12-30 12:31:54 +0200

    Unsupported browsers

--- public/snow.js
+++ public/snow.js
@@ -16,6 +16,14 @@ $(document).ready( function() {
     }).on("webcamSwipeRight", function () {
         $("canvas.snow").trigger("letItSnow.set", ["windPower", 25]);
     });
+
+    var $info = $(".info");
+    var isChrome = /chrom(e|ium)/.test(navigator.userAgent.toLowerCase());
+    if (!navigator.getUserMedia || !isChrome) {
+       $info.html("Unfortunately, this is <strong>not</strong> supported in your browser... <i class='fa fa-meh-o'></i> Please reopen the page in a recent version of <strong>Chrome browser</strong>."); 
+    } else {
+       $info.html("Move your right hand in front of the webcam to change the direction of the snow.");
+    }
 });
 
 // init the webcam swiper
